1e935901127612bbf6a9a026fdb14c000d873d5b
InkyChew <inky11308@gmail.com>
2026-07-27 16:22:20 +0800

    CMS網站配置

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,14 +1,16 @@
+import siteData from "@/data/site.json";
+
 export const site = {
-    name: '勇氣實驗室',
-    description: '我是誰？我不知道。但我想透過紀錄來認識自己。',
+    name: siteData.name,
+    description: siteData.description,
     locale: 'zh-TW',
-    author: 'Inky',
+    author: siteData.author,
     url: `${process.env.NEXT_PUBLIC_SITE_URL || 'https://yourdomain.com'}`,
     logo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-    email: 'address@yoursite.com',
-    youtube: 'https://youtube.com',
-    threads: 'https://www.threads.net',
-    instagram: 'https://www.instagram.com',
+    email: siteData.email,
+    youtube: siteData.youtube,
+    threads: siteData.threads,
+    instagram: siteData.instagram,
     analytics: {
         googleAnalytics: {
             googleAnalyticsId: process.env.NEXT_PUBLIC_GA_ID
